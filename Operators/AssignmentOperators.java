1e7fd559e78d1bec53a3cd358d0aaaab78af2a61
subukavya <kavyasubu1207@gmail.com>
2026-02-08 09:25:37 +0530

    Add AssignmentOperators class with examples

---
+++ Operators/AssignmentOperators.java
@@ -0,0 +1,9 @@
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        a += 5;    // a = 15
+        a -= 3;    // a = 12
+        a *= 2;    // a = 24
+        System.out.println("Final a: " + a);
+    }
+}
