167e44b16af6a8b664625aa2609e8e5a7ffdc489
Kavya N <kavyasubu1207@gmail.com>
2026-02-11 09:37:34 +0530

    Add IfElse class to check voting eligibility
    Implement if-else statement to check voting eligibility based on age.

---
+++ Control_statements/IfElse.java
@@ -0,0 +1,13 @@
+public class IfElse {
+    public static void main(String[] args) {
+
+        int age = 18;
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+}
+// The if-else statement is used to execute code based on a true or false condition.
