5b7b9e0874c0eb64a3f8a3f9e4d0e0ecc7b993ab
Kavya N <kavyasubu1207@gmail.com>
2026-02-21 09:21:48 +0530

    Implement hashCode method in Employee class
    Added a hashCode method to the Employee class that returns the employee's ID as the hash value.

--- ObjectClassMethods/3_HashCodeDemo.java
+++ ObjectClassMethods/3_HashCodeDemo.java
@@ -1,1 +1,25 @@
 
+/*
+ * hashCode()
+ * ----------
+ * Returns integer hash value of object.
+ * Used in hashing-based collections like HashMap.
+ * If equals() is overridden, hashCode() should also be overridden.
+ */
+
+class Employee {
+    int id;
+
+    Employee(int id) {
+        this.id = id;
+    }
+
+    public int hashCode() {
+        return id;
+    }
+
+    public static void main(String[] args) {
+        Employee e = new Employee(101);
+        System.out.println(e.hashCode());
+    }
+}
