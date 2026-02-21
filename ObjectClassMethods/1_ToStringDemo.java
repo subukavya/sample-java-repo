677539e100c62c49aaeea24feb827184ffd23154
Kavya N <kavyasubu1207@gmail.com>
2026-02-21 09:20:58 +0530

    Implement Student class with custom toString() method
    Added a Student class with a custom toString() method to provide a meaningful string representation of the object.

--- ObjectClassMethods/1_ToStringDemo.java
+++ ObjectClassMethods/1_ToStringDemo.java
@@ -1,1 +1,28 @@
 
+/*
+ * toString()
+ * ----------
+ * Used to return a string representation of an object.
+ * It is automatically called when we print an object.
+ * Default: prints class name + hashcode.
+ * We override it to print meaningful data.
+ */
+
+class Student {
+    int id;
+    String name;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString() {
+        return id + " - " + name;
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student(1, "Ravi");
+        System.out.println(s);  // Calls toString()
+    }
+}
